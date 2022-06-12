670434e9f04c1320a3436e15971f5b0a4b2da10e
Fajar <fajardwi@its.ac.id>
2022-06-12 21:40:25 +0700

    Assign Challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try{
+    let jumlah = 0
+    const filmIxx = await promiseTheaterIXX();
+    const filmVGC = await promiseTheaterVGC();
+    filmIxx.forEach(element => {
+      if(element.hasil == emosi){
+        jumlah += 1;
+      }
+    });
+    filmVGC.forEach(element => {
+      if(element.hasil == emosi){
+        jumlah += 1;
+      }
+    });
+
+    return jumlah;
+
+  }catch(rejectMessage){
+    console.log(rejectMessage);
+  }
+}
 
 module.exports = {
   promiseOutput,
